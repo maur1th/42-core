132856939042704280b21c4db4713bec02655c3b
Thomas Maurin <maurin.th@gmail.com>
2015-11-29 15:55:41 +0100

    updated pretty_test

--- libft/pretty_test.c
+++ libft/pretty_test.c
@@ -22,7 +22,7 @@ int valid = 0;
 
 void test(const char *desc) {
 	if (count != 0) {
-		printf("  %d/%d test passed\n", valid, count);
+		printf("  %d/%d tests passed\n", valid, count);
 	}
 	valid = 0;
 	count = 0;
@@ -49,6 +49,9 @@ void assert(const char *desc, int value, int expect) {
 #define STR_NORMAL_LEN 10
 #define STR_FEDUP "test\0test"
 #define STR_FEDUP_LEN 9
+#define STR_HAY "ababca"
+#define STR_HAY_LEN 7
+
 
 int main() {
 
@@ -238,7 +241,7 @@ int main() {
 	dummy_ptr1 = ft_memccpy(dummy_str1, STR_NORMAL, STR_NORMAL[4], STR_NORMAL_LEN);
 	dummy_ptr2 = memccpy(dummy_str2, STR_NORMAL, STR_NORMAL[4], STR_NORMAL_LEN);
 	assert("contents indentical to system", memcmp(dummy_str1, dummy_str2, 4), 0);
-	assert("returned indentical to system", (int)dummy_ptr1, (int)dummy_ptr2);
+	assert("returns s1", (int)(dummy_ptr1), (int)dummy_str1);
 
 	dummy_ptr1 = ft_memccpy(dummy_str1, STR_NORMAL, -1, STR_NORMAL_LEN);
 	assert("returns NULL when done", (int)dummy_ptr1, (int)NULL);
@@ -250,16 +253,16 @@ int main() {
 	strcpy(dummy_str1, STR_NORMAL);
 	strcpy(dummy_str2, STR_NORMAL);
 	ft_memmove(dummy_str1 + 1, dummy_str1, STR_NORMAL_LEN);
-	memmove(dummy_str1 + 1, dummy_str1, STR_NORMAL_LEN);
-	assert("contents indentical to system", memcmp(dummy_str1, dummy_str2, STR_NORMAL_LEN + 1), 0);
+	memmove(dummy_str2 + 1, dummy_str2, STR_NORMAL_LEN);
+	assert("contents indentical to system", memcmp(dummy_str1, dummy_str2, STR_NORMAL_LEN), 0);
 
 	dummy_str1 = malloc(STR_NORMAL_LEN * 2 * sizeof(*dummy_str1));
 	dummy_str2 = malloc(STR_NORMAL_LEN * 2 * sizeof(*dummy_str2));
 	strcpy(dummy_str1, STR_NORMAL);
 	strcpy(dummy_str2, STR_NORMAL);
 	ft_memmove(dummy_str1, dummy_str1 + 1, STR_NORMAL_LEN - 1);
-	memmove(dummy_str1, dummy_str1 + 1, STR_NORMAL_LEN - 1);
-	assert("contents indentical to system", memcmp(dummy_str1, dummy_str2, STR_NORMAL_LEN - 1), 0);
+	memmove(dummy_str2, dummy_str2 + 1, STR_NORMAL_LEN - 1);
+	assert("contents indentical to system", memcmp(dummy_str1, dummy_str2, STR_NORMAL_LEN), 0);
 
 	/////////////////////////////////////////////////////////////
 	test("ft_memchr");
@@ -328,6 +331,91 @@ int main() {
 	 dummy_int1 = ft_strncmp(STR_NORMAL, STR_FEDUP, STR_NORMAL_LEN);
 	 dummy_int2 = strncmp(STR_NORMAL, STR_FEDUP, STR_NORMAL_LEN);
 	 assert("different strings, result indentical to system", dummy_int1, dummy_int2);
+
+	 /////////////////////////////////////////////////////////////
+	 test("ft_strstr");
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strstr(dummy_str1, STR_EMPTY);
+	assert("returns original when s2 is empty", (int)dummy_ptr1, (int)dummy_str1);
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strstr(dummy_str1, "win");
+	dummy_ptr2 = ft_strstr(dummy_str1, "win");
+	assert("finds s2 in s1", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strstr(dummy_str1, "winkle\0lavander");
+	dummy_ptr2 = strstr(dummy_str1, "winkle\0lavander");
+	assert("f'ed up s2", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strstr(dummy_str1, "waldo");
+	dummy_ptr2 = strstr(dummy_str1, "waldo");
+	assert("returns NULL when s2 not found", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strstr(STR_NORMAL, "winlkeo");
+	dummy_ptr2 = strstr(STR_NORMAL, "winlkeo");
+	assert("returns NULL when s2 matches to end of s1 but hasn't ended yet", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_HAY_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_HAY);
+	dummy_ptr1 = ft_strstr(dummy_str1, "abc");
+	dummy_ptr2 = strstr(dummy_str1, "abc");
+	assert("finds s2 when false start", (int)dummy_ptr1, (int)dummy_ptr2);
+
+
+	/////////////////////////////////////////////////////////////
+	 test("ft_strnstr");
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strnstr(dummy_str1, STR_EMPTY, STR_NORMAL_LEN);
+	assert("returns noriginal when s2 is empty", (int)dummy_ptr1, (int)dummy_str1);
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strnstr(dummy_str1, "win", STR_NORMAL_LEN);
+	dummy_ptr2 = strnstr(dummy_str1, "win", STR_NORMAL_LEN);
+	assert("finds s2n in s1", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strnstr(dummy_str1, "winkle\0lavander", STR_NORMAL_LEN);
+	dummy_ptr2 = strnstr(dummy_str1, "winkle\0lavander", STR_NORMAL_LEN);
+	assert("f'ed up s2", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strnstr(dummy_str1, "waldo", STR_NORMAL_LEN);
+	dummy_ptr2 = strnstr(dummy_str1, "waldo", STR_NORMAL_LEN);
+	assert("returns NULL when s2 not found", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_NORMAL_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_NORMAL);
+	dummy_ptr1 = ft_strnstr(STR_NORMAL, "winlkeo", STR_NORMAL_LEN);
+	dummy_ptr2 = strnstr(STR_NORMAL, "winlkeo", STR_NORMAL_LEN);
+	assert("returns NULL when s2 matches to end of s1 but hasn't ended yet", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_HAY_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_HAY);
+	dummy_ptr1 = ft_strnstr(dummy_str1, "abc", STR_HAY_LEN);
+	dummy_ptr2 = strnstr(dummy_str1, "abc", STR_HAY_LEN);
+	assert("finds s2 when false start", (int)dummy_ptr1, (int)dummy_ptr2);
+
+	dummy_str1 = malloc(STR_HAY_LEN * sizeof(*dummy_str1));
+	strcpy(dummy_str1, STR_HAY);
+	dummy_ptr1 = ft_strnstr(dummy_str1, "abc", 3);
+	dummy_ptr2 = strnstr(dummy_str1, "abc", 3);
+	assert("doesn't find b/c has char restraint", (int)dummy_ptr1, (int)dummy_ptr2);
+
+
 
 	test(NULL);
 
