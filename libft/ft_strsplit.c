919fb9414345514aa7260f992a4883fea42899ea
Thomas Maurin <maurin.th@gmail.com>
2015-11-30 23:02:03 +0100

    strsplit strtrim

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,17 +6,64 @@
 /*   By: tm <tm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 00:49:23 by tm                #+#    #+#             */
-/*   Updated: 2015/11/30 00:51:53 by tm               ###   ########.fr       */
+/*   Updated: 2015/11/30 23:00:09 by tm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int		count_words(const char *s, char c)
+{
+	int		i;
+
+	i = 0;
+	if (*s == '\0')
+		return (0);
+	if (*(s++) != c)
+		i += 1;
+	while (*s != '\0')
+	{
+		if (*s != c && *(s - 1) == c)
+			i += 1;
+		s += 1;
+	}
+	return (i);
+}
+
+static char		*get_word(const char *s, char c)
+{
+	char	*buf;
+	int		i;
+
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+		i += 1;
+	buf = (char *)malloc(sizeof(*buf) * i + 1);
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		buf[i] = s[i];
+		i += 1;
+	}
+	buf[i] = '\0';
+	return (buf);
+}
+
 char	**ft_strsplit(char const *s, char c)
 {
 	char	**tab;
-	tab = (char**)malloc(sizeof(char*) * 5);
-	(void)s;
-	(void)c;
-	return tab;
+	int		i;
+
+	tab = (char **)malloc(sizeof(char*) * (count_words(s, c) + 1));
+	i = 0;
+	if (*s && *s != c)
+		tab[i++] = get_word(s++, c);
+	while (*s != '\0')
+	{
+		if (*s != c && *(s - 1) == c)
+			tab[i++] = get_word(s, c);
+		s += 1;
+	}
+	tab[i] = NULL;
+	return (tab);
 }
