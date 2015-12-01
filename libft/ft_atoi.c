166ab6976c06ab7051f05666acc77b320e427921
Thomas Maurin <maurin.th@gmail.com>
2015-12-01 21:28:51 +0100

    removed unnecessary code

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,27 +6,20 @@
 /*   By: tmaurin <tmaurin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 17:22:40 by tmaurin           #+#    #+#             */
-/*   Updated: 2015/12/01 20:22:56 by tmaurin          ###   ########.fr       */
+/*   Updated: 2015/12/01 21:25:27 by tmaurin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isspace(char c)
-{
-	if (c <= 32)
-		return (1);
-	return (0);
-}
-
 int			ft_atoi(const char *str)
 {
 	long	result;
-	int		sign;
+	short	sign;
 
 	sign = 1;
 	result = 0;
-	if (ft_strlen(str) == 0 || (*str < 32 && !ft_isspace(*str)))
+	if (ft_strlen(str) == 0)
 		return (0);
 	while (*str >= 0 && *str <= 32)
 		str++;
@@ -37,6 +30,6 @@ int			ft_atoi(const char *str)
 		str++;
 	}
 	while (*str >= '0' && *str <= '9')
-		result = (result + *str++ - '0') * 10;
+		result = (result + *(str++) - '0') * 10;
 	return (result / 10 * sign);
 }
