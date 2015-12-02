28785d4385fd9ff122e165082dc495e4f2c51b48
Thomas Maurin <maurin.th@gmail.com>
2015-12-02 14:47:20 +0100

    optim memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -3,27 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmaurin <tmaurin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tm <tm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 16:26:28 by tmaurin           #+#    #+#             */
-/*   Updated: 2015/11/28 16:40:31 by tmaurin          ###   ########.fr       */
+/*   Updated: 2015/12/02 14:46:29 by tm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_memcmp(const void *s1x, const void *s2x, size_t n)
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*s1;
-	unsigned char	*s2;
-
-	s1 = (unsigned char*)s1x;
-	s2 = (unsigned char*)s2x;
 	while (n-- != 0)
 	{
-		if (*s1 != *s2)
+		if (*(unsigned char*)s1 != *(unsigned char*)s2)
 		{
-			return (*s1 - *s2);
+			return (*(unsigned char*)s1 - *(unsigned char*)s2);
 		}
 		s1 += 1;
 		s2 += 1;
--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmaurin <tmaurin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tm <tm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/08 15:32:37 by tmaurin           #+#    #+#             */
-/*   Updated: 2015/11/28 18:17:26 by tmaurin          ###   ########.fr       */
+/*   Updated: 2015/12/02 14:46:17 by tm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int		ft_strcmp(const char *s1, const char *s2)
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 != *s2)
-			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+			return (*(unsigned char*)s1 - *(unsigned char*)s2);
 		s1 += 1;
 		s2 += 1;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return (*(unsigned char*)s1 - *(unsigned char*)s2);
 }
