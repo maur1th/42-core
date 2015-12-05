d44742846880e9f40a39d8232805ee2757d7e3e8
Thomas Maurin <maurin.th@gmail.com>
2015-12-05 12:30:22 +0100

    fixed const char* return

--- fillit/libft/ft_itoa_base.c
+++ fillit/libft/ft_itoa_base.c
@@ -6,11 +6,12 @@
 /*   By: tmaurin <tmaurin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 21:09:24 by tmaurin           #+#    #+#             */
-/*   Updated: 2015/12/05 10:32:15 by tmaurin          ###   ########.fr       */
+/*   Updated: 2015/12/05 12:29:34 by tmaurin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 static void		convert(int value, int base, char *s, short *i)
 {
@@ -50,7 +51,7 @@ char			*ft_itoa_base(int value, int base)
 
 	if (value == -2147483648)
 	{
-		return ("-2147483648");
+		return (ft_strdup("-2147483648"));
 	}
 	sign = value < 0 && base == 10 ? 1 : 0;
 	value = value < 0 ? -value : value;
