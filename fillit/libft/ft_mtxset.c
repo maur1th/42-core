7f0e17f9c49ba337a9cd7d7b28ffa83f338fd43f
Thomas Maurin <maurin.th@gmail.com>
2015-12-11 18:52:20 +0100

    fixed check size

--- fillit/libft/ft_mtxset.c
+++ fillit/libft/ft_mtxset.c
@@ -6,7 +6,7 @@
 /*   By: tm <tm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 17:45:26 by tm                #+#    #+#             */
-/*   Updated: 2015/12/11 18:40:14 by tm               ###   ########.fr       */
+/*   Updated: 2015/12/11 18:50:04 by tm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_mtx	*ft_mtxset(t_mtx *mtx, size_t row, size_t col, char value)
 {
 	char	*data;
 
-	if (row * col > mtx->height * mtx->width)
+	if (row * col > (mtx->height - 1) * (mtx->width - 1))
 		return NULL;
 	data = mtx->data;
 	data[row + col * mtx->width] = value;
