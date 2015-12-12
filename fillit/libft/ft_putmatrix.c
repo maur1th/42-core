99efc1e4d12fcc87d5678cc0cfd7498e568751c2
Thomas Maurin <maurin.th@gmail.com>
2015-12-12 12:15:41 +0100

    renamed var

--- fillit/libft/ft_putmatrix.c
+++ fillit/libft/ft_putmatrix.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putmtx.c                                        :+:      :+:    :+:   */
+/*   ft_putmatrix.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tm <tm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 20:12:55 by tm                #+#    #+#             */
-/*   Updated: 2015/12/12 12:09:35 by tm               ###   ########.fr       */
+/*   Updated: 2015/12/12 12:14:55 by tm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,17 @@
 
 void	ft_putmatrix(t_matrix *matrix)
 {
-	char	*d0;
+	char	*orig;
 	char	*data;
 
-	d0 = matrix->data;
-	data = d0;
+	orig = matrix->data;
+	data = orig;
 	while (*data != '\0')
 	{
 		ft_putchar(*(data++));
-		if ((data - d0) % matrix->width == 0)
-		{
+		if ((data - orig) % matrix->width == 0)
 			ft_putchar('\n');
-		}
 		else
-		{
 			ft_putchar(' ');
-		}
 	}
 }
