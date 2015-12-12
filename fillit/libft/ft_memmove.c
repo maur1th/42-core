9c485e0eae2c2c03fcd169fa25f0964bb0f36ed0
Thomas Maurin <maurin.th@gmail.com>
2015-12-12 12:04:49 +0100

    fixed memmove

--- fillit/libft/ft_memmove.c
+++ fillit/libft/ft_memmove.c
@@ -3,20 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmaurin <tmaurin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tm <tm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 16:04:28 by tmaurin           #+#    #+#             */
-/*   Updated: 2015/12/01 20:23:20 by tmaurin          ###   ########.fr       */
+/*   Updated: 2015/12/12 11:49:09 by tm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	tmp[len];
+	char	*tmp;
 
+	if(!(tmp = (char*)malloc(sizeof(char) * len)))
+		return (NULL);
 	ft_memcpy(tmp, src, len);
 	ft_memcpy(dst, tmp, len);
+	free(tmp);
 	return (dst);
 }
