5156183eb37fbdc3ce150d5119367078b15dd939
Thomas Maurin <maurin.th@gmail.com>
2016-01-26 19:00:17 +0100

    Fixed insert tetr

--- fillit/srcs/insert_tetrimino.c
+++ fillit/srcs/insert_tetrimino.c
@@ -6,7 +6,7 @@
 /*   By: tm <tm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 11:01:50 by tm                #+#    #+#             */
-/*   Updated: 2016/01/26 18:25:30 by tm               ###   ########.fr       */
+/*   Updated: 2016/01/26 18:59:06 by tm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,41 @@ static t_bool	check_size(t_matrix *square, t_matrix *tetr, int pos)
 	return (true);
 }
 
-t_bool			insert_tetrimino(t_matrix *square, t_matrix *tetr, int pos)
+static t_bool	insert_valid(t_matrix *square, t_matrix *tetr, int pos)
 {
 	int		i;
 	int		row;
 	int		col;
-	char	*backup;
 
-	if (!check_size(square, tetr, pos))
-		return (false);
-	backup = ft_strdup(square->data);
 	i = -1;
 	while (tetr->data[++i] != '\0')
 	{
 		row = pos / square->width + i / tetr->width;
 		col = (pos + i % tetr->width) % square->width;
-		if (ft_matrixget(square, row, col) != '0' && tetr->data[i] != '0')
-		{
-			free(square->data);
-			square->data = backup;
+		if (ft_matrixget(square, row, col) == '0')
+			ft_matrixset(square, row, col, tetr->data[i]);
+		else if (tetr->data[i] != '0')
 			return (false);
-		}
-		ft_matrixset(square, row, col, tetr->data[i]);
 	}
-	ft_putchar('\n');
-	free(backup);
 	return (true);
+}
+
+t_bool			insert_tetrimino(t_matrix *square, t_matrix *tetr, int pos)
+{
+	char	*backup;
+
+	if (!check_size(square, tetr, pos))
+		return (false);
+	backup = ft_strdup(square->data);
+	if (insert_valid(square, tetr, pos))
+	{
+		free(backup);
+		return (true);
+	}
+	else
+	{
+		free(square->data);
+		square->data = backup;
+		return (false);
+	}
 }
