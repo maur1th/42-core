0d4ef9c476cf0f30654bd1535265d25d5e5ebf4c
Thomas Maurin <maurin.th@gmail.com>
2016-01-27 21:06:08 +0100

    Less mem alloc, more computation

--- fillit/srcs/insert_tetrimino.c
+++ fillit/srcs/insert_tetrimino.c
@@ -6,7 +6,7 @@
 /*   By: tm <tm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 11:01:50 by tm                #+#    #+#             */
-/*   Updated: 2016/01/26 18:59:06 by tm               ###   ########.fr       */
+/*   Updated: 2016/01/27 21:03:21 by tm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,24 @@ static t_bool	check_size(t_matrix *square, t_matrix *tetr, int pos)
 	return (true);
 }
 
-static t_bool	insert_valid(t_matrix *square, t_matrix *tetr, int pos)
+static t_bool	check_insert(t_matrix *square, t_matrix *tetr, int pos)
+{
+	int		i;
+	int		row;
+	int		col;
+
+	i = -1;
+	while (tetr->data[++i] != '\0')
+	{
+		row = pos / square->width + i / tetr->width;
+		col = (pos + i % tetr->width) % square->width;
+		if (ft_matrixget(square, row, col) != '0' && tetr->data[i] != '0')
+			return (false);
+	}
+	return (true);
+}
+
+static void		insert(t_matrix *square, t_matrix *tetr, int pos)
 {
 	int		i;
 	int		row;
@@ -36,28 +53,13 @@ static t_bool	insert_valid(t_matrix *square, t_matrix *tetr, int pos)
 		col = (pos + i % tetr->width) % square->width;
 		if (ft_matrixget(square, row, col) == '0')
 			ft_matrixset(square, row, col, tetr->data[i]);
-		else if (tetr->data[i] != '0')
-			return (false);
 	}
-	return (true);
 }
 
 t_bool			insert_tetrimino(t_matrix *square, t_matrix *tetr, int pos)
 {
-	char	*backup;
-
-	if (!check_size(square, tetr, pos))
+	if (!check_size(square, tetr, pos) || !check_insert(square, tetr, pos))
 		return (false);
-	backup = ft_strdup(square->data);
-	if (insert_valid(square, tetr, pos))
-	{
-		free(backup);
-		return (true);
-	}
-	else
-	{
-		free(square->data);
-		square->data = backup;
-		return (false);
-	}
+	insert(square, tetr, pos);
+	return (true);
 }
