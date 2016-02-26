f0cdf9856192808577e8143af0004333e999ce7a
Tbouder-mac <tbouder@student.42.fr>
2016-02-26 20:53:42 +0100

    up app names

--- link/link/portfolio/apps.py
+++ link/link/portfolio/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class AssetsConfig(AppConfig):
-    name = 'assets'
+class PortfolioConfig(AppConfig):
+    name = 'portfolio'
