d2ff0fc72f75d3aa207b8b99ce215fde5747af02
Tbouder-mac <tbouder@student.42.fr>
2016-02-26 22:27:51 +0100

    Removing tabs 2

--- link/link/customer/models.py
+++ link/link/customer/models.py
@@ -3,31 +3,31 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
-	lastname = models.CharField(max_length = 50)
-	firstname = models.CharField(max_length = 50)
-	sex = models.CharField(max_length = 50)
-	birthdate = models.DateField()
-	status = models.CharField(max_length = 50)
-	job = models.CharField(max_length = 50)
-	company = models.CharField(max_length = 50)
-	children = models.DecimalField(max_digits=2, decimal_places=0)
-	area = models.CharField(max_length = 50)
-	profile = models.CharField(max_length = 50)
-	start_date_relation		= models.DateField()
+    lastname = models.CharField(max_length = 50)
+    firstname = models.CharField(max_length = 50)
+    sex = models.CharField(max_length = 50)
+    birthdate = models.DateField()
+    status = models.CharField(max_length = 50)
+    job = models.CharField(max_length = 50)
+    company = models.CharField(max_length = 50)
+    children = models.DecimalField(max_digits=2, decimal_places=0)
+    area = models.CharField(max_length = 50)
+    profile = models.CharField(max_length = 50)
+    start_date_relation = models.DateField()
 
-	def __str__(self):
-		return "{} {}".format(self.nom, self.prenom)
+    def __str__(self):
+        return "{} {}".format(self.nom, self.prenom)
 
 class Account(models.Model):
-	client = models.ForeignKey(Client, related_name='accounts')
-	initial_date = models.DateField(max_length = 50)
-	initial_amount = models.DecimalField(max_digits=30, decimal_places=15)
-	final_amount = models.DecimalField(max_digits=30, decimal_places=15)
-	account_type = models.CharField(max_length = 50)
-	profile = models.CharField(max_length = 50)
+    client = models.ForeignKey(Client, related_name='accounts')
+    initial_date = models.DateField(max_length = 50)
+    initial_amount = models.DecimalField(max_digits=30, decimal_places=15)
+    final_amount = models.DecimalField(max_digits=30, decimal_places=15)
+    account_type = models.CharField(max_length = 50)
+    profile = models.CharField(max_length = 50)
 
-	def __str__(self):
-		return self.client
+    def __str__(self):
+        return self.client
 
 # from customer.models import *
 # account = Account.objects.get(id=1)
