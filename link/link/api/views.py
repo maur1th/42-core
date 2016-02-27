175a543725318200dc44c3bb5e582b3cefed68f1
Thomas Maurin <maurin.th@gmail.com>
2016-02-27 19:36:26 +0100

    Add Product and Composition to REST API

--- link/link/api/views.py
+++ link/link/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, filters
 from customer.models import Client, Account
-from portfolio.models import Analytics
-from .serializers import ClientSerializer, AccountSerializer,\
-    AnalyticsSerializer
+from portfolio.models import Analytics, Composition
+from product.models import Product
+from .serializers import ClientSerializer, AccountSerializer, \
+    AnalyticsSerializer, CompositionSerializer, ProductSerializer
 
 
 class ClientViewSet(viewsets.ModelViewSet):
@@ -19,3 +20,14 @@ class AnalyticsViewSet(viewsets.ModelViewSet):
     queryset = Analytics.objects.all()
     serializer_class = AnalyticsSerializer
     filter_fields = ('account', )
+
+
+class CompositionViewSet(viewsets.ModelViewSet):
+    queryset = Composition.objects.all()
+    serializer_class = CompositionSerializer
+    filter_fields = ('account', 'date', )
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
