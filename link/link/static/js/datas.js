fd1801ef49157319d0c6fde167289004d0029c2b
Tbouder-mac <tbouder@student.42.fr>
2016-02-27 23:27:18 +0100

    datas

--- link/link/static/js/datas.js
+++ link/link/static/js/datas.js
@@ -1,24 +1,63 @@
 jQuery(function ($) {
   var clientid = $('#chart1').data('clientid');
-  $.get('/api/accounts/?client=' + clientid, function (res) {
-    res.results.forEach(function (account) {
-      console.log('/api/analytics/?account=' + account.id);
-      $.get('/api/analytics/?account=' + account.id, function (res) {
-        console.log(res);
-      }).fail(function (err) { console.log(err); });
-    });
-  });
-  $.get('/api/analytics/?account=422', function (res) {
-    console.log(res);
-  });
+  var length = 0;
+  var today = moment().format('DD/MM/YYYY');
+  var tmp1;
+  var tmp2;
+
+
   var data1 = [1300000, 1300000 + 44787.9476113673, 1300000 + 44787.9476113673 + 80916.8145389641, 1300000 + 44787.9476113673 + 80916.8145389641 + 98769.5744280268, 1300000 + 44787.9476113673 + 80916.8145389641 + 98769.5744280268 + -94378.3334875151];
   var data2 = [3, 9, 12, 14, 22, 32, 45, 12, 67, 45, 55, 7];
   var data3 = [23, 19, 11, 134, 242, 352, 435, 22, 637, 445, 555, 57];
   var data4 = [10, 20, 30, 40, 50];
-
+  var data5 = [1];
   var data6 = [87, 100 - 87];
 
-  var today = moment().format('DD/MM/YYYY');
+  function datas(data) {
+    $("#chart5").shieldChart({
+      exportOptions: {
+        image: false,
+        print: false
+      },
+      primaryHeader: {
+        text: 'Variation journalière',
+        align: 'center'
+      },
+      dataSeries: [{
+        seriesType: "line",
+        data: data,
+        color: 'red'
+      }],
+    });
+  };
+
+  $.get('/api/accounts/?client=' + clientid, function (res)
+  {
+    res.results.forEach(function (account)
+    {
+        console.log(res.results);
+
+      $.get('/api/analytics/?account=' + account.id, function (res)
+      {
+        data5 = [];
+        length = res.results.length - 2;
+        // console.log(res.results.length);
+        res.results.forEach(function (pl, i)
+        {
+          if (pl.period !== "Global")
+          {
+            data5[length] = parseInt(pl.account_PL);
+            length -= 1;
+          }
+        });
+        if (!tmp1)
+          tmp1 = data5;
+
+        datas(data5);
+      });
+    });
+  });
+
 
 
   $(function () {
@@ -87,21 +126,21 @@ jQuery(function ($) {
     //  }]
     // });
 
-    $("#chart5").shieldChart({
-      exportOptions: {
-        image: false,
-        print: false
-      },
-      primaryHeader: {
-        text: 'Variation journalière',
-        align: 'center'
-      },
-      dataSeries: [{
-        seriesType: "line",
-        data: data5,
-        color: 'red'
-      }],
-    });
+    // $("#chart5").shieldChart({
+    //   exportOptions: {
+    //     image: false,
+    //     print: false
+    //   },
+    //   primaryHeader: {
+    //     text: 'Variation journalière',
+    //     align: 'center'
+    //   },
+    //   dataSeries: [{
+    //     seriesType: "line",
+    //     data: data5,
+    //     color: 'red'
+    //   }],
+    // });
 
     $("#chart6").shieldChart({
       exportOptions: {
