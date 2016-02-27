3c474ae8ef8d7520bec8617da695dc1377d24073
Thomas MAURIN <maurin.th@gmail.com>
2016-02-27 23:06:42 +0100

    Fixed incorrect default route

--- link/link/link/urls.py
+++ link/link/link/urls.py
@@ -9,7 +9,7 @@ from django.conf import settings
 
 urlpatterns = patterns(
     '',
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('customer:top'),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('customer:login'),
         permanent=True)),
     url(r'^customer/', include('customer.urls', namespace='customer')),
     url(r'^api/', include('api.urls', namespace='api')),
