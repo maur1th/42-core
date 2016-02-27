50c3edb0972077bb9b1dbfe96889e29da2b4ecf1
Thomas Maurin <maurin.th@gmail.com>
2016-02-27 18:44:02 +0100

    removed useless field

--- link/link/api/serializers.py
+++ link/link/api/serializers.py
@@ -3,18 +3,10 @@ from rest_framework import serializers
 
 
 class ClientSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name="api:client-detail",
-    )
-
     class Meta:
         model = Client
 
 
 class AccountSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name="api:account-detail",
-    )
-
     class Meta:
         model = Account
