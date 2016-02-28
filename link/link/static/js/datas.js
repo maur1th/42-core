bf06943cc71b68b5dad529188a73de8e0bbdf225
Tbouder-mac <tbouder@student.42.fr>
2016-02-28 01:12:16 +0100

    graphs

--- link/link/static/js/datas.js
+++ link/link/static/js/datas.js
@@ -1,10 +1,8 @@
 jQuery(function ($) {
   var clientid = $('#chart1').data('clientid');
+  var nb_accounts = 0;
   var length = 0;
   var today = moment().format('DD/MM/YYYY');
-  var tmp1;
-  var tmp2;
-
 
   var data1 = [1300000, 1300000 + 44787.9476113673, 1300000 + 44787.9476113673 + 80916.8145389641, 1300000 + 44787.9476113673 + 80916.8145389641 + 98769.5744280268, 1300000 + 44787.9476113673 + 80916.8145389641 + 98769.5744280268 + -94378.3334875151];
   var data2 = [3, 9, 12, 14, 22, 32, 45, 12, 67, 45, 55, 7];
@@ -13,8 +11,8 @@ jQuery(function ($) {
   var data5 = [1];
   var data6 = [87, 100 - 87];
 
-  function datas(data) {
-    $("#chart5").shieldChart({
+  function drawChart(dates, series) {
+    $('#chart1').shieldChart({
       exportOptions: {
         image: false,
         print: false
@@ -23,46 +21,74 @@ jQuery(function ($) {
         text: 'Variation journalière',
         align: 'center'
       },
-      dataSeries: [{
-        seriesType: "line",
-        data: data,
-        color: 'red'
-      }],
+      axisX: {
+        categoricalValues: dates
+      },
+      dataSeries: series
     });
   };
 
-  $.get('/api/accounts/?client=' + clientid, function (res)
-  {
-    res.results.forEach(function (account)
-    {
-        console.log(res.results);
-
-      $.get('/api/analytics/?account=' + account.id, function (res)
-      {
-        data5 = [];
-        length = res.results.length - 2;
-        // console.log(res.results.length);
-        res.results.forEach(function (pl, i)
-        {
-          if (pl.period !== "Global")
-          {
-            data5[length] = parseInt(pl.account_PL);
-            length -= 1;
+  $.get('/api/accounts/?client=' + clientid, function (accounts) {
+    var dates = [];
+    var series = [];
+    var dataSeries = [];
+    accounts.results.forEach(function (account) {
+      $.get('/api/analytics/?account=' + account.id, function (res) {
+        var serie = [];
+        res.results.forEach(function (analytics) {
+          if (analytics.period !== 'Global') {
+            serie.push(parseInt(analytics.account_PL));
+            dates.push(analytics.period);
           }
         });
-        if (!tmp1)
-          tmp1 = data5;
-
-        datas(data5);
+        series.push(serie);
+        if (series.length === accounts.count) {
+          series.forEach(function (serie) {
+            dataSeries.push({
+              seriesType: 'line',
+              data: serie,
+              collectionAlias: account.account_type
+            });
+          });
+        }
+      }).done(function () {
+        drawChart(dates, dataSeries);
       });
     });
   });
 
+  // $.get('/api/accounts/?client=' + clientid, function (accounts) {
+  //   var dates = [];
+  //   var series = [];
+  //   var dataSeries = [];
+  //   accounts.results.forEach(function (account) {
+  //     $.get('/api/analytics/?account=' + account.id, function (res) {
+  //       var serie = [];
+  //       res.results.forEach(function (analytics) {
+  //         if (analytics.period !== 'Global') {
+  //           serie.push(parseInt(analytics.account_PL));
+  //           dates.push(analytics.period);
+  //         }
+  //       });
+  //       series.push(serie);
+  //     }).done(function () {
+  //       series.forEach(function (serie) {
+  //         dataSeries.push({
+  //           seriesType: 'line',
+  //           data: serie,
+  //           collectionAlias: account.account_type
+  //         });
+  //       });
+  //       console.log(JSON.stringify(dataSeries));
+  //       drawChart(dates, dataSeries);
+  //     });
+  //   });
+  // });
 
 
   $(function () {
-    $(".links a").shieldTooltip();
-    $("#chart1").shieldChart({
+    $('.links a').shieldTooltip();
+    $('#chart5').shieldChart({
       exportOptions: {
         image: false,
         print: false
@@ -73,34 +99,34 @@ jQuery(function ($) {
       },
       axisY: {
         title: {
-          text: "Break-Down for selected quarter"
+          text: 'Break-Down for selected quarter'
         },
       },
       axisX: {
-        categoricalValues: ["23/08/2013", "31/12/2013", "31/12/2014", "31/12/2015", today]
+        categoricalValues: ['1', '2', '3', '4', today]
       },
       dataSeries: [{
-        seriesType: "line",
+        seriesType: 'line',
         data: data1,
-        collectionAlias: "P&L"
-        // color: "red"
+        collectionAlias: 'P&L'
+        // color: 'red'
       }]
     });
-    // $("#chart4").shieldChart({
+    // $('#chart4').shieldChart({
     //  exportOptions: {
     //    image: false,
     //    print: false
     //  },
     //  dataSeries: [{
-    //    seriesType: "line",
+    //    seriesType: 'line',
     //    data: data2
     //  }, {
-    //    seriesType: "line",
+    //    seriesType: 'line',
     //    data: data3
     //  }]
     // });
 
-    $("#chart3").shieldChart({
+    $('#chart3').shieldChart({
       exportOptions: {
         image: false,
         print: false
@@ -110,23 +136,23 @@ jQuery(function ($) {
         align: 'center'
       },
       dataSeries: [{
-        seriesType: "donut",
+        seriesType: 'donut',
         data: data4
       }]
     });
 
-    // $("#chart2").shieldChart({
+    // $('#chart2').shieldChart({
     //  exportOptions: {
     //    image: false,
     //    print: false
     //  },
     //  dataSeries: [{
-    //    seriesType: "donut",
+    //    seriesType: 'donut',
     //    data: data5
     //  }]
     // });
 
-    // $("#chart5").shieldChart({
+    // $('#chart5').shieldChart({
     //   exportOptions: {
     //     image: false,
     //     print: false
@@ -136,19 +162,19 @@ jQuery(function ($) {
     //     align: 'center'
     //   },
     //   dataSeries: [{
-    //     seriesType: "line",
+    //     seriesType: 'line',
     //     data: data5,
     //     color: 'red'
     //   }],
     // });
 
-    $("#chart6").shieldChart({
+    $('#chart6').shieldChart({
       exportOptions: {
         image: false,
         print: false
       },
       dataSeries: [{
-        seriesType: "donut",
+        seriesType: 'donut',
         data: data5
       }]
     });
